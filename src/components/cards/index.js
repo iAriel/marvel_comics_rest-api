314a861bf23625b3060e2f6896ea1af3f9f9ce48
iAriel <ariel@aluno.fapce.edu.br>
2021-11-24 21:54:09 -0300

    marvel comic modal details done

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -1,5 +1,19 @@
 import {React, useEffect, useState} from 'react';
-import { Box } from "@chakra-ui/react"
+
+import {
+    Box,
+    Spinner,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    Button,
+    useDisclosure
+} from "@chakra-ui/react"
+
 import axios from 'axios';
 import md5 from 'md5';
 
@@ -12,44 +26,73 @@ export default function Cards (){
     const time = Number(new Date());
     const hash = md5(time + privateKey+publicKey);
     
-    
     const [comics,setComics] = useState([])
     const [isLoading, setLoading] = useState(true);
+    const [comicDetails, setComicDetails] = useState({})
+    const { isOpen, onOpen, onClose } = useDisclosure();
     
     useEffect( () =>{
-        axios(`http://gateway.marvel.com/v1/public/characters?ts=${time}&apikey=${publicKey}&hash=${hash}`).then((response) => {
+        axios(`http://gateway.marvel.com/v1/public/comics?ts=${time}&apikey=${publicKey}&hash=${hash}`).then((response) => {
             setComics(response.data.data.results)
-            console.log(comics)
+            setLoading(false)
         }).catch(err => {
             console.log(err)
         })
-    }, [])
+    }, [time, publicKey, hash])
 
+    function setInfoModal (comic){
+        setComicDetails(comic)
+        onOpen()
+    }
 
     return(
         <div>
             <div className="title">
                 <h1>OUR COMICS</h1>
             </div>
-
-            <div className="cards">
-                {comics.map(comic =>(
-                    <Box display="flex" alignItems="baseline">
-                       <img src={comic.thumbnail.path+"/portrait_xlarge.jpg"} />
-                       <Box
-                            color="gray.500"
-                            fontWeight="semibold"
-                            letterSpacing="wide"
-                            fontSize="xs"
-                            textTransform="uppercase"
-                            ml="2"
-                        >
-                            <p>{comic.name}</p> 
-                        </Box>
-                    </Box>
+            {isLoading ? (
+                <div className="load">
+                    <Spinner color="red.500" size="xl"/>
+                </div>
+            ):(
+                <div className="cards" >
+                {comics.map((comic, index) =>(
+                    <div key={index} >
+                        <button onClick={() => setInfoModal(comic)}>
+                            <Box display="flex" alignItems="baseline" >
+                                <img src={comic.thumbnail.path+"/portrait_xlarge.jpg"} alt={comic.title}/>
+                                <Box
+                                        color="gray.500"
+                                        fontWeight="semibold"
+                                        letterSpacing="wide"
+                                        fontSize="xs"
+                                        textTransform="uppercase"
+                                        ml="2"
+                                    >
+                                    <p>{comic.title}</p> 
+                                </Box>
+                            </Box>
+                        </button>
+                    </div>
                 ))}
             </div>
-            
+            )}
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>{comicDetails.title}</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        {comicDetails.description || "No description"}
+                    </ModalBody>
+                            
+                    <ModalFooter>
+                        <Button colorScheme="blue" mr={3} onClick={onClose}>
+                                Close
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </div>
         
     )
